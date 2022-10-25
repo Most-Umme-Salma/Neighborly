2c7db454e73f1a5b130b935b616712fdc4cbb8fb
Cristina Caran <cristinacaran@Cristinas-Air.fritz.box>
2022-10-25 11:19:59 +0200

    front and back- end connected, CORS enabled, fe-routing works, got distracted and created some backend routes and middlewares as well. Also installed other 3rd party middlewares.

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,2 @@
 const mongoose = require("mongoose")
-
-
 mongoose.connect(process.env.CONNECTION_STRING).catch((err)=>{console.log(err)});
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 require("./db");
-const express= require("express");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 const { postRouter } = require("./routes/posts")
 const { authRouter } = require("./routes/auth")
 
@@ -9,13 +11,26 @@ const app = express();
 require("./controllers/posts")
 //console.log(process.env.CONNECTION_STRING)
 const port = process.env.PORT || 3001;
+
+
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
 app.get("/", (req,res)=>{
     res.send("Testing get request")
 });
+app.get("/cors", (req,res)=>{
+    res.set('Access-Controll-Allow-Origin', '*')
+    res.send({"msg" : "This should have Cors enabled"})
+});
 
-app.use(express.json())
-app.listen(port,()=>{console.log(`Server is running on http://localhost:${port}`)});
-
+//routers
 app.use("/posts", postRouter);
 app.use("/auth", authRouter);
+
+app.listen(port,()=>{console.log(`Server is running on http://localhost:${port}`)});
+
+
  
