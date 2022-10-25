e4927ff50d31842a84c77069c12a473c5b5eae1e
Umme Salma <nupur2238@gmail.com>
2022-10-25 19:19:15 +0200

    created footer section

--- fe-neighborly/src/App.js
+++ fe-neighborly/src/App.js
@@ -1,26 +1,29 @@
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home';
-import Newoffer from './components/new-offer/Newoffer';
-import Offer from './components/offers/Offers';
-import Promotions from './components/promotions/Promotions';
-import Searchbar from './components/searchbar/Searchbar';
-import {Route, Routes} from "react-router-dom"
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Newoffer from "./components/new-offer/Newoffer";
+import Offer from "./components/offers/Offers";
+import Promotions from "./components/promotions/Promotions";
+import Searchbar from "./components/searchbar/Searchbar";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div >
-      <Navbar/>
-      <div>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/offer' element={<Offer/>}/>
-          <Route path='/newoffer' element={<Newoffer/>}/>
-          <Route path='/promotions' element={<Promotions/>}/>
-          <Route path='/searchbar' element={<Searchbar/>}/>
-
-        </Routes>
+    <div className="page-container">
+      <div className="content-wrap">
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/offer" element={<Offer />} />
+            <Route path="/newoffer" element={<Newoffer />} />
+            <Route path="/promotions" element={<Promotions />} />
+            <Route path="/searchbar" element={<Searchbar />} />
+          </Routes>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
--- fe-neighborly/src/components/navbar/Navbar.js
+++ fe-neighborly/src/components/navbar/Navbar.js
@@ -1,5 +1,4 @@
-import { Children } from "react";
-import { NavLink, Link, useMatch, useResolvedPath } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import "./navbar.css";
 import { BsFillCartDashFill, BsBellFill } from "react-icons/bs";
 
