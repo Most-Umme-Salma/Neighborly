a25edf72ff4a8dd3328a688c1ddf05d0633e1599
Umme Salma <nupur2238@gmail.com>
2022-10-27 18:09:07 +0200

    create category dropdown button and update backend category

--- fe-neighborly/src/App.js
+++ fe-neighborly/src/App.js
@@ -1,40 +1,46 @@
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home';
-import Newoffer from './components/new-offer/Newoffer';
-import Offer from './components/offers/Offers';
-import Promotions from './components/promotions/Promotions';
-import Searchbar from './components/searchbar/Searchbar';
-import Login from './components/login/Login';
-import {Route, Routes} from "react-router-dom"
-import {useEffect, useState} from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Newoffer from "./components/new-offer/Newoffer";
+import Offer from "./components/offers/Offers";
+import Promotions from "./components/promotions/Promotions";
+import Searchbar from "./components/searchbar/Searchbar";
+import Login from "./components/login/Login";
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import Category from "./components/category/Category";
 
-function App(){
+function App() {
   const [posts, setPosts] = useState();
-  useEffect(()=>{
+  const [selected, setSelected] = useState("");
+  useEffect(() => {
     axios
       .get("http://localhost:3001")
-      .then((res)=>{setPosts(res.data)})
-      .catch((err)=>{console.log(err)})
-  },[])
-  
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div >
-      <Navbar/>
+    <div>
+      <Navbar />
       <div>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/offer' element={<Offer/>}/>
-          <Route path='/newoffer' element={<Newoffer/>}/>
-          <Route path='/promotions' element={<Promotions/>}/>
-          <Route path='/searchbar' element={<Searchbar/>}/>
-          <Route path='/login' element={<Login />}/>
-
+          <Route path="/" element={<Home />} />
+          <Route path="/offer" element={<Offer />} />
+          <Route path="/newoffer" element={<Newoffer />} />
+          <Route path="/promotions" element={<Promotions />} />
+          <Route path="/searchbar" element={<Searchbar />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
+        <Category selected={selected} setSelected={setSelected} />
       </div>
     </div>
   );
-};
+}
 
 export default App;
--- fe-neighborly/src/components/category/Category.js
+++ fe-neighborly/src/components/category/Category.js
@@ -1,5 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import "./category.css";
+import { BsCaretDownFill } from "react-icons/bs";
 
-export default function Category() {
-  return <div></div>;
+export default function Category({ selected, setSelected }) {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <div className="dropdown">
+      <div className="dropdown-dtn " onClick={(e) => setIsActive(!isActive)}>
+        <div>Categories</div>
+        <div>
+          <BsCaretDownFill size={15} />
+        </div>
+      </div>
+
+      {isActive && (
+        <div className="dropdown-content">
+          <div className="dropdown-items">
+            <div
+              onClick={(e) => {
+                setSelected(e.target.posts);
+                setIsActive(false);
+              }}
+            >
+              All
+            </div>
+          </div>
+
+          <div className="dropdown-items">
+            <div
+              onClick={(e) => {
+                setSelected(e.target.food);
+                setIsActive(false);
+              }}
+            >
+              Hobby + Services
+            </div>
+          </div>
+          <div className="dropdown-items">
+            <div
+              onClick={(e) => {
+                setSelected(e.target.services);
+                setIsActive(false);
+              }}
+            >
+              Household + Services
+            </div>
+          </div>
+          <div className="dropdown-items">
+            <div
+              onClick={(e) => {
+                setSelected(e.target.services);
+                setIsActive(false);
+              }}
+            >
+              NewOffer
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
+
+/*<div className="container-fluid max-2">
+      <div className="row mt-5 max-2">
+        <div className="col-md-2">
+          <button className="btn  w-100 mb-3">Food</button>
+          <button className="btn w-100">Services</button>
+        </div>
+      </div>
+    </div>*/
